03ae19e2fe5ef61e302be631c68ea5b2f9390fa2
NouraAlfouzan <441201224@student.ksu.edu.sa>
2022-10-14 23:00:15 +0300

    color

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
       child: SafeArea(
         child: Container(
