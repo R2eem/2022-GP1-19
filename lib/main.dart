bdf52f078cf7122f7f2568313a2e1ca9b0569049
shahadalmasoud <shahad.m.almasoud@gmail.com>
2022-10-15 00:15:38 +0300

    shahad first

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.pink,
         body: SingleChildScrollView(
       child: SafeArea(
         child: Container(
