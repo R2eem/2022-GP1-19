a5b505e7753b0affa59a3a52f9a2c0e6c840a72a
ShahadAmasoud1 <shahad.m.almasoud@gmail.com>
2022-10-16 15:36:14 +0300

    last Non-precription category

--- lib/NonPrescriptionCategory.dart
+++ lib/NonPrescriptionCategory.dart
@@ -200,7 +200,7 @@ class NonPrescription extends State<NonPrescriptionCategory> {
     );
   }
 
-  //query method
+  //query methood
   Future<List<ParseObject>> getNonPresMedication() async {
     QueryBuilder<ParseObject> queryNonPresMedication =
     QueryBuilder<ParseObject>(ParseObject('Medication'));
