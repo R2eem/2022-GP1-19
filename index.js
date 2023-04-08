466802b3ceb87f1dc11d0a9f20a49c6c54a2fe1f
lamaBaabdullah <98309043+lamaBaabdullah@users.noreply.github.com>
2023-04-09 01:54:43 +0300

    lama last copy

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-const sideMenu = document.querySelector("aside");
+const sidemenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
-const closeBtn = document.querySelector("#close-btn")
-const themeToggler = document.querySelector(".theme-toggler");
+const closeBtn = document.querySelector("#close-btn");
 
-//Show sidbar
-menuBtn.addEventListener('click',() =>{
-    sideMenu.style.display='block';
+
+function menuFunction() {
+    sidemenu.style.display='block';
+  }
+
+closeBtn.addEventListener('click',()=>{
+    sidemenu.style.display='none';
 })
-
-//close sidbar
-closeBtn.addEventListener('click',() =>{
-    sideMenu.style.display='none';
-})
-
-//change theme 
-themeToggler.addEventListener('click',() =>{
-    document.body.classList.toggle('dark-theme-variables');
-
-    themeToggler.querySelector('i:nth-child(1)').classList.toggle('active');
-    themeToggler.querySelector('i:nth-child(2)').classList.toggle('active');
-
-
-
-})
-
-
-
